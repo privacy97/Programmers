6e28d7a730d8a5917b7a13ef3af98691bf94b6ae
privacy97 <89842901+privacy97@users.noreply.github.com>
2023-01-26 23:10:54 +0900

    [level 0] Title: 369게임, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120891. 369게임/369게임.py
@@ -0,0 +1,8 @@
+def solution(order):
+    order = list(str(order))
+    answer = 0
+    answer += order.count('3')
+    answer += order.count('6')
+    answer += order.count('9')
+    
+    return answer
