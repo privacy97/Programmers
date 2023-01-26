f09460d70951ba933a819bd086e5e8eee86e5ecd
privacy97 <89842901+privacy97@users.noreply.github.com>
2023-01-26 23:11:02 +0900

    [level 0] Title: 합성수 찾기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.py
@@ -0,0 +1,10 @@
+def solution(n):
+    answer = 0
+    
+    for i in range(1,n+1):
+        for j in range(2, int(i**(1/2))+1):
+            if i % j == 0:
+                answer += 1
+                break
+                
+    return answer
